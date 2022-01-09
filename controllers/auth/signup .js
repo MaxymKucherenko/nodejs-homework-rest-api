340eff659d1b1ec201a35bc966704cd12b6c9905
MaxymKucherenko <maxym_88@ukr.net>
2022-01-09 22:18:32 +0200

    first commit

---
+++ controllers/auth/signup .js
@@ -0,0 +1,42 @@
+const { Conflict } = require('http-errors')
+const gravatar = require('gravatar')
+const { nanoid } = require('nanoid')
+
+const { sendEmail } = require('../../helpers')
+const { User } = require('../../models')
+
+const signup = async (req, res) => {
+  const { name, email, password } = req.body
+  const user = await User.findOne({ email })
+  if (user) {
+    throw new Conflict(`User with ${email} already exist`)
+  }
+  const verificationToken = nanoid()
+  const avatarURL = gravatar.url(email)
+  const newUser = new User({ name, email, avatarURL, verificationToken })
+  newUser.setPassword(password)
+  await newUser.save()
+
+  const mail = {
+    to: email,
+    subject: 'Confirm email',
+    html: `<a target='_blank' href='http://localhost:3000/api/users/verify/${verificationToken}'>Click for email confirmation</a>`,
+  }
+
+  await sendEmail(mail)
+
+  res.status(201).json({
+    status: 'success',
+    code: 201,
+    data: {
+      user: {
+        email,
+        name,
+        avatarURL,
+        verificationToken,
+      },
+    },
+  })
+}
+
+module.exports = signup
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,25 @@
 const express = require('express')
+
+const { validation, ctrlWrapper, auth } = require('../../middlewares')
+const { joiSchema, favoriteJoiSchema } = require('../../models/contact')
+const { contacts: ctrl } = require('../../controllers')
+
 const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/', auth, ctrlWrapper(ctrl.listContacts))
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/:id', ctrlWrapper(ctrl.getById))
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post('/', auth, validation(joiSchema), ctrlWrapper(ctrl.add))
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete('/:id', ctrlWrapper(ctrl.removeById))
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put('/:id', validation(joiSchema), ctrlWrapper(ctrl.updateById))
+
+router.patch(
+  '/:id/favorite',
+  validation(favoriteJoiSchema),
+  ctrlWrapper(ctrl.updateStatusContact)
+)
 
 module.exports = router

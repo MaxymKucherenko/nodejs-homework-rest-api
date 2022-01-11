ca3a79504ee707d31cc972ed22d7b65f9f7e42a1
MaxymKucherenko <maxym_88@ukr.net>
2022-01-12 00:25:11 +0200

    fix: await

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,19 +1,20 @@
-const sgMail = require('@sendgrid/mail')
-require('dotenv').config()
+const sgMail = require('@sendgrid/mail');
+require('dotenv').config();
 
-const { SENDGRID_API_KEY } = process.env
+const { SENDGRID_API_KEY } = process.env;
 
-sgMail.setApiKey(SENDGRID_API_KEY)
+sgMail.setApiKey(SENDGRID_API_KEY);
 
 const sendEmail = async (data) => {
   const email = { ...data, from: 'k.maxym21@gmail.com' }
-  await sgMail
-    .send(email)
-    .then(() => {
-      console.log('Email has been sended')
-      return true
-    })
-    .catch((e) => console.log(e.message))
+  try {
+    await sgMail.send(email)
+    console.log('Email has been sended')
+    return true
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
 }
 
 module.exports = sendEmail
